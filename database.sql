f1646fe90f26ebff37a54ba137eef872c0cabf09
“ericelvendahl” <ericelvendahl@gmail.com>
2020-08-04 15:23:12 -0500

    first joined query functional

--- database.sql
+++ database.sql
@@ -8,3 +8,60 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+-- 4. Which warehouses have diet pepsi?
+SELECT "warehouse"."warehouse" 
+	FROM "warehouse" 
+		JOIN "warehouse_product" 
+		ON "warehouse"."id" = "warehouse_product"."warehouse_id" 
+			JOIN "products" 
+			ON "warehouse_product"."product_id" = "products"."id" 
+				WHERE "description" = 'diet pepsi';
+
+                CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(60),
+    last_name VARCHAR(80)
+);
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(255),
+    city VARCHAR(60),
+    state VARCHAR(2),
+    zip VARCHAR(12),
+    address_type VARCHAR(8),
+    customer_id integer REFERENCES customers
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    order_date date,
+    address_id integer REFERENCES addresses
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255),
+    unit_price numeric(3,2)
+);
+
+CREATE TABLE line_items (
+    id SERIAL PRIMARY KEY,
+    quantity integer,
+    order_id integer REFERENCES orders,
+    product_id integer REFERENCES products
+);
+
+CREATE TABLE warehouse (
+    id SERIAL PRIMARY KEY,
+    warehouse VARCHAR(55),
+    fulfillment_days integer
+);
+
+CREATE TABLE warehouse_product (
+    product_id integer NOT NULL REFERENCES products,
+    warehouse_id integer NOT NULL REFERENCES warehouse,
+    on_hand integer,
+    PRIMARY KEY (product_id, warehouse_id)
+);
