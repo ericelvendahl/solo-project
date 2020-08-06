875a5e59668d206aa4f53fc497d95350167cf269
“ericelvendahl” <ericelvendahl@gmail.com>
2020-08-06 14:30:01 -0500

    DOM elements for add task

--- src/components/CurrentTaskListPage/CurrentTaskListPage.js
+++ src/components/CurrentTaskListPage/CurrentTaskListPage.js
@@ -4,22 +4,63 @@ import LogOutButton from "../LogOutButton/LogOutButton";
 
 class CurrentTaskListPage extends Component {
   componentDidMount() {}
+
+  state = {
+    showAddComponent: false,
+    taskName: "",
+    taskDescription: "",
+  };
+
+  addClicked = () => {
+    console.log("in addClicked");
+    this.props.dispatch({
+      type: "ADD_TASK",
+      payload: {
+        name: this.state.taskName,
+        description: this.state.taskDescription,
+      },
+    });
+  };
+
+  handleInputChangeFor = (propertyName) => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  }; // end handleInputChangeFor
+
   linkClicked = (thisID) => {
     // console.log("In linkClicked, thisID is", thisID);
     // this.props.dispatch({ type: "FETCH_LIST_BY_ID", payload: thisID });
-  };
-  // this component doesn't do much to start, just renders some user info to the DOM
+  }; // end linkClicked
+
   render() {
     return (
-      <div>
-        <LogOutButton className="log-in" />
-        <p>Current task list: {JSON.stringify(this.props.currentTaskList)}</p>
-        {this.props.currentTaskList.map((x, key) => (
-          <p key={key}>
-            {x.task_name}
-          </p>
-        ))}
-        <p>User data is {JSON.stringify(this.props.user)}</p>
+      <div className="parent">
+        <div className="child-span-12">
+          Task Name:
+          <input
+            type="text"
+            name="taskName"
+            value={this.state.taskName}
+            onChange={this.handleInputChangeFor("taskName")}
+          />
+          Description:
+          <input
+            type="text"
+            name="taskDescription"
+            value={this.state.taskDescription}
+            onChange={this.handleInputChangeFor("taskDescription")}
+          />
+          <button className="log-in" onClick={this.addClicked}>
+            Add task
+          </button>
+          {this.showAddComponent ? <p>true</p> : <p>false</p>}
+          <p>Current task list: {JSON.stringify(this.props.currentTaskList)}</p>
+          {this.props.currentTaskList.map((x, key) => (
+            <p key={key}>{x.task_name}</p>
+          ))}
+          <p>User data is {JSON.stringify(this.props.user)}</p>
+        </div>
       </div>
     );
   }
